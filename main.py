b3adf5f3030faf886c59a618db55e4ec8c24eae1
zhuyi <zhuyicode@gmail.com>
2026-04-27 16:09:30 +0800

    no debug

--- main.py
+++ main.py
@@ -123,4 +123,4 @@ def health_db(db=Depends(get_db)):
 
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True, )
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=False, )
